7097214d9aaa5573da7ffe44edcee0b9ee5e46a1
aminelli <56305052+aminelli@users.noreply.github.com>
2024-09-13 16:01:15 +0200

    update

--- ES003/src/App.tsx
+++ ES003/src/App.tsx
@@ -2,7 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+export default class App extends React.Component {
+
+  componentDidUpdate(prevProps: Readonly<{}>, prevState: Readonly<{}>, snapshot?: any): void {
+    console.log("APP -> componentDidUpdate");
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+    console.log("APP -> componentDidCatch");
+  }
+
+  componentDidMount(): void {
+    console.log("APP -> componentDidMount");
+  }
+
+  componentWillUnmount(): void {
+    console.log("APP -> componentWillUnmount");
+  }
+
+
+  forceUpdate(callback?: (() => void) | undefined): void {
+    console.log("APP -> forceUpdate");
+  }
+
+  render() {
+     console.log("APP -> render");
+    return (
+      <>
+        Test
+      </>
+    );
+  }
+
+}
+
+/*
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -24,3 +61,4 @@ function App() {
 }
 
 export default App;
+*/
